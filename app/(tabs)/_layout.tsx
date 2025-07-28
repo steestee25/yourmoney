efa2eb7f7cb7062763d693843341e1e075481d63
Stefano <s.molari1@campus.unimib.it>
2025-07-28 19:17:12 +0200

    Add bottom navigation

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,37 @@
+import { Tabs } from 'expo-router';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+export default function TabLayout() {
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: '#007bff',
+                headerStyle: {
+                    backgroundColor: '#fff',
+                },
+                headerShadowVisible: false,
+                headerTintColor: '#007bff',
+                tabBarStyle: {
+                    backgroundColor: '#fff',
+                },
+            }}
+        >
+            <Tabs.Screen name="index"
+                options={{
+                    title: 'YourMoney',
+                    tabBarIcon: ({ color, focused }) => (
+                        <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+                    ),
+                }} />
+            <Tabs.Screen
+                name="about"
+                options={{
+                    title: 'About',
+                    tabBarIcon: ({ color, focused }) => (
+                        <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24} />
+                    ),
+                }}
+            />
+        </Tabs>
+    );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,9 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
 
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: "Home" }} />
-      <Stack.Screen name="about" options={{ title: "About" }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
   );
 }
